d5e4419ae5b5113ffd43d9791210a3e52583722e
marangY <jwkid9124@gmail.com>
2021-11-03 17:48:07 +0900

    11/3

---
+++ main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//nav color change
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+    if (window.scrollY > navbarHeight){
+        navbar.classList.add('navbar--dark');
+    } else{
+        navbar.classList.remove('navbar--dark');
+    }
+});
+
+// nav move
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    scrollIntoView(link);
+});
+
+// contactme
+const homeContact = document.querySelector(".home__contact");
+homeContact.addEventListener("click", (event) => {
+    scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior : "smooth"});
+}
